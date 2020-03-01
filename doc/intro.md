1929a065d6045d5bab1c42f46add82677e6d5acb
lifeng <87260280@qq.com>
2020-03-01 16:26:43 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to perf-test-clj-clore-async
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/perf_test_clj_clore_async/core.clj
@@ -0,0 +1,7 @@
+(ns perf-test-clj-clore-async.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
